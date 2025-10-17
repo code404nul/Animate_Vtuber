257c5bd5cc89ce3a11746d9309b212b668f7f6b2
arch <enrsalut@gmail.com>
2025-10-17 07:27:40 +0200

    Delete useless file

--- test2.py
+++
@@ -1,3 +0,0 @@
-import utils.get_emotion
-
-print(utils.get_emotion.analyse_texte("I love to have meeting at 3am."))
--- test3.py
+++
@@ -1,5 +0,0 @@
-from utils.manage_model import ModelManager
-
-model = ModelManager("mao")
-
-print([i["Name"] for i in model.expressions])
